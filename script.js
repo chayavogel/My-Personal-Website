8702a99b775e4ca1125b08961db16464ba7d67a6
chaya <chayavogel1@gmail.com>
2023-07-09 12:50:23 -0400

    change animation gif

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("portrait");
 
 const firstImage = "profile.jpg";
-const secondImage = "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/funny-face-giraffe-sheila-brown.jpg";
+const secondImage = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd2VzeTEydWhiZmNhNGxiamZlc2I0dWp5N2JiNHc0bTcwcWhkdzJyciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ASJ2k35r0ynm/giphy.gif";
 
 portrait.addEventListener("mouseover", function (event) {
     if (event = "focus") {
